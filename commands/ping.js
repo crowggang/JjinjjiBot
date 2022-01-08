4df4797cd6ccbc0f2384a995b9c252ed375a2a26
Hong Ki Yong <crowggang@gmail.com>
2022-01-08 22:48:24 +0900

    interaction

---
+++ commands/ping.js
@@ -0,0 +1,15 @@
+const {CommandInteraction} = require('discord.js');
+module.exports = {
+    name : 'ping2',
+    description : 'ping 답변',
+    /*
+    *  @params {CommandInteraction} interaction
+    * */
+
+    async execute(interaction){
+        await interaction.reply('pong');
+
+    }
+
+
+}
--- index.js
+++ index.js
@@ -44,21 +44,40 @@
     // app.listen(port, () => {
     //     console.log('Express server listening on port', port)
     // });
-    const { Client, Intents } = require('discord.js');
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    const { Client, Intents, Collection} = require('discord.js');
+    const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
+    const fs = require('fs');
+
+    const commandFiles = fs.readdirSync('./commands').fill(file => file.endsWith('.js'));
+    client.commands = new Collection();
+    let data = [];
+    for(const file of commandFiles){
+        const command = require('./commands/${file}');
+        client.commands.set(command.name , command);
+        data.push({name:command.name , description : command.description ,options : command.options });
+    }
+
+    client.once('ready' , async () => {
+       console.log('ready!');
+       await  client.guilds.cache.get('910721739498061874')?.commands.set(data);
+    });
 
     client.on('ready', () => {
         console.log(`Logged in as ${client.user.tag}!`);
     });
+    client.on('interactionCreate', async interaction => {
+        if(!interaction.isCommand()) return;
 
-    client.on('interactionCreate', async (interaction) => {
-        if (!interaction.isCommand()) return;
-
-            console.debug('aaaa');
-        if (interaction.commandName === 'ping2') {
-            await interaction.reply('Pong!');
+        if(!client.commands.has(interaction.commandName)) return;
+        const command = client.commands.get(interaction.commandName);
+        try {
+            await command.execute(interaction);
+        }catch (error){
+            console.log('error!!' , error);
         }
+
     });
 
+  
     // client.login(config.token);
     client.login(process.env.BOT_TOKEN);
