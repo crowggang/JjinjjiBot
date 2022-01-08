d7978ba5901fc6a7726826f6ab5910fbea19d455
Hong Ki Yong <crowggang@gmail.com>
2022-01-08 20:31:33 +0900

    port

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
     const client = new Discord.Client();
     const config = require('./config.json');
 
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => {
+        console.log('Express server listening on port', port)
+    });
+
     client.on('ready', () => {
         console.log('theclient');
     });
