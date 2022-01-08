b2f9ad3221d4750d94dbfd6a2b5c9d78fedffbe3
Hong Ki Yong <crowggang@gmail.com>
2022-01-08 22:54:03 +0900

    aaaa

--- index.js
+++ index.js
@@ -1,58 +1,14 @@
-    // require('./bot.js');
-    // require('./web.js');
 
-    //
-    //
-    // const port = process.env.PORT || 8080;
-    // const express = require('express');
-    // const packageInfo = require('./package.json');
-    //
-    // const Discord = require('discord.js');
-    //
-    // const client = new Discord.Client({
-    //     intents: [ // Intent를 설정합니다. 설정하지 않으면 CLIENT_MISSING_INTENTS 오류가 발생합니다.
-    //         Intents.FLAGS.GUILDS,
-    //         Intents.FLAGS.GUILD_MESSAGES
-    //     ]
-    // }); // Discord.Client 객체를 생성합니다.
     const config = require('./config.json');
-    //
-    //
-    // let app = express();
-    //
-    // app.get('/', function (req, res) {
-    //     res.json({ version: packageInfo.version });
-    // });
-    //
-    // var server = app.listen(process.env.PORT, function () {
-    //     var host2 = server.address().address;
-    //     var port2 = server.address().port;
-    //
-    //     console.log('Web server started at http://%s:%s', host2, port2);
-    // });
-    //
-    //
-    //
-    //
-    // client.on('ready', () => {
-    //     console.log('theclient');
-    // });
-    //
-    // // client.login(process.env.BOT_TOKEN);
-    // client.login(config.token);
-    //
-    // app.listen(port, () => {
-    //     console.log('Express server listening on port', port)
-    // });
     const { Client, Intents, Collection} = require('discord.js');
     const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
     const fs = require('fs');
 
-    const commandFiles = fs.readdirSync('./commands').fill(file => file.endsWith('.js'));
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
     client.commands = new Collection();
     let data = [];
     for(const file of commandFiles){
-        const command = require('./commands/${file}');
+        const command = require(`./commands/${file}`);
         client.commands.set(command.name , command);
         data.push({name:command.name , description : command.description ,options : command.options });
     }
@@ -78,6 +34,6 @@
 
     });
 
-  
+
     // client.login(config.token);
     client.login(process.env.BOT_TOKEN);
