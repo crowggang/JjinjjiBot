cd61e54567ea8921df363eb8534578abf75bf392
Hong Ki Yong <crowggang@gmail.com>
2022-01-10 23:31:48 +0900

    커밋

--- commands/minchul.js
+++ commands/minchul.js
@@ -2,6 +2,6 @@ module.exports = {
     name:"민철",
     description: "민철",
     execute(message){
-        return message.channel.send('민철이 키........17..?');
+        return message.channel.send('민철이 키........170..? \n???????????????????');
     }
 }
