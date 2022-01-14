535c6014c01a028a6020b1c4be2d104f586273ed
Hong Ki Yong <crowggang@gmail.com>
2022-01-14 18:18:30 +0900

    윤주21

--- commands/yunju.js
+++ commands/yunju.js
@@ -2,6 +2,6 @@ module.exports = {
     name:"윤주",
     description: "윤주",
     execute(message){
-        return message.channel.send('잡앗쥬??? \n 이지하쥬??? \n 이겻쥬??? \n 쉽다쉬워 >ㅁ<!!!!!!!!! 벽느끼지마~~');
+        return message.channel.send(`잡앗쥬???ㅋ \n이지하쥬???ㅋ\n이겻쥬???ㅋ\n 쉽다쉬워  벽느껴버렸쥬? 'ㅠ' ^ㅁ^!!!!!!!!!  `);
     }
 }
