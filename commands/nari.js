f6f06af301e61eaa426730277717c071f5b08dd4
Hong Ki Yong <crowggang@gmail.com>
2022-01-14 17:46:44 +0900

    오류수정

--- commands/nari.js
+++ commands/nari.js
@@ -2,6 +2,6 @@ module.exports = {
     name:"나리",
     description: "나리",
     execute(message){
-        return message.channel.send('나리야... 속박 좋아행???????? \n > ㅁ<');
+        return message.channel.send('나리야... 속박 좋아행???????? \n >ㅁ <');
     }
 }
