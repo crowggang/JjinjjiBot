47baa6751694b8240b8a36d16f4a8deb90a62424
Hong Ki Yong <crowggang@gmail.com>
2022-01-14 18:27:41 +0900

    PTSD

---
+++ commands/kyungmin.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name:"경민",
+    description: "경민",
+    execute(message){
+        return message.channel.send(`영선아 민철아 제발 사고 좀 치지마 이색기들아 내가 이렇게 까지 얘기 해야겠니???? 어??!!!! 어!? 아 녹턴 !!!개새기야!!!  `);
+    }
+}
