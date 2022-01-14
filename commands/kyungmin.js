72d3257041773b4828148ea643e4fa9a5d8f30d9
Hong Ki Yong <crowggang@gmail.com>
2022-01-14 18:31:41 +0900

    ㅇㅇㅇ

--- commands/kyungmin.js
+++ commands/kyungmin.js
@@ -2,6 +2,6 @@ module.exports = {
     name:"경민",
     description: "경민",
     execute(message){
-        return message.channel.send(`영선아 민철아 제발 사고 좀 치지마 이색기들아 내가 이렇게 까지 얘기 해야겠니???? 어??!!!! 어!? 아 녹턴 !!!개새기야!!!  `);
+        return message.channel.send(`영선아 민철아 제발 사고 좀 치지마 이색기들아 내가 이렇게 까지 얘기 해야겠니???? 어??!!!! 어!? 아 녹턴 !!! 왜 나한테만 궁 쓰냐고!! `);
     }
 }
